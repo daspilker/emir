edf1b57bb1d041bdc2f8f5deeb255018bc536bc1
Daniel Spilker <mail@daniel-spilker.com>
2010-10-28 21:59:11 +0200

    better window title

--- UI/Converters.cs
+++ UI/Converters.cs
@@ -10,7 +10,7 @@ namespace EmIR.UI
     {
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            return "EmIR " + (value == null ? "" : "- " + value) + "- " + App.CurrentApp.Version;
+            return "EmIR " + App.CurrentApp.Version + (string.IsNullOrEmpty(value as string) ? "" : " - " + value);
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
